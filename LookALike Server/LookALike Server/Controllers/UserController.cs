5510c20ce45c51d22b99d2591e5c00a8383089ad
YakirC0412 <155883698+YakirC0412@users.noreply.github.com>
2024-04-16 11:47:28 +0300

    First Push

---
+++ LookALike Server/LookALike Server/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using LookALike_Server.Class;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LookALike_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            User user = new User();
+            return user.Read();
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public int Post([FromBody] User user)
+        {
+            int NumberOfInsert = user.Insert();
+            return NumberOfInsert;
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{Email}")]
+        public int Put([FromBody] User u)
+        {
+            return u.UpdateUser();
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LookALike Server/LookALike Server/DAL/DBservices.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+using LookALike_Server.Class;
+//using RuppinProj.Models;
+
+/// <summary>
+/// DBServices is a class created by me to provides some DataBase Services
+/// </summary>
+public class DBservices
+{
+
+    public DBservices()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    // This method creates a connection to the database according to the connectionString name in the web.config 
+    //--------------------------------------------------------------------------------------------------
+    public SqlConnection connect(String conString)
+    {
+
+        // read the connection string from the configuration file
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json").Build();
+        string cStr = configuration.GetConnectionString("myProjDB");
+        SqlConnection con = new SqlConnection(cStr);
+        con.Open();
+        return con;
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    // This method Inserts a User to the Users table 
+    //--------------------------------------------------------------------------------------------------
+    public int Insert(User user)
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+
+        try
+        {
+            con = connect("myProjDB"); // create the connection
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        cmd = CreateUserInsertCommandWithStoredProcedure("sp_LAL_InsertUser", con, user);  // create the command
+
+        try
+        {
+            int numEffected = cmd.ExecuteNonQuery(); // execute the command
+            return numEffected;
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        finally
+        {
+            if (con != null)
+            {
+                // close the db connection
+                con.Close();
+            }
+        }
+
+    }
+
+    //---------------------------------------------------------------------------------
+    // Create the SqlCommand for Insert User using a stored procedure
+    //---------------------------------------------------------------------------------
+    private SqlCommand CreateUserInsertCommandWithStoredProcedure(String spName, SqlConnection con, User user)
+    {
+
+        SqlCommand cmd = new SqlCommand(); // create the command object
+
+        cmd.Connection = con;              // assign the connection to the command object
+
+        cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+        cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+        cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be text
+
+        cmd.Parameters.AddWithValue("@Email", user.Email);
+        cmd.Parameters.AddWithValue("@First_name", user.FirstName);
+        cmd.Parameters.AddWithValue("@Last_name", user.LastName);
+        cmd.Parameters.AddWithValue("@Image", user.Image);
+        cmd.Parameters.AddWithValue("@Phone_Number", user.PhoneNumber);
+        cmd.Parameters.AddWithValue("@Date_of_Birth", user.DateOfBirth);
+        cmd.Parameters.AddWithValue("@Password", user.Password);
+
+        return cmd;
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    // This method reads Users from the database 
+    //--------------------------------------------------------------------------------------------------
+    public List<User> ReadUsers()
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+        List<User> UsersList = new List<User>();
+
+        try
+        {
+            con = connect("myProjDB"); // create the connection
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        cmd = CreateUserCommandWithStoredProcedureWithoutParameters("sp_LAL_ReadUsers", con);   // create the command
+
+        try
+        {
+            SqlDataReader dataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+            while (dataReader.Read())
+            {
+                User u = new User();
+                u.Email = dataReader["Email"].ToString();
+                u.FirstName = dataReader["First_Name"].ToString();
+                u.LastName = dataReader["Last_Name"].ToString();
+                u.Image = dataReader["Image"].ToString();
+                u.PhoneNumber = Convert.ToInt32(dataReader["Phone_Number"]);
+                // Retrieve Date_of_birth as DateTime
+                DateTime dateOfBirth = (DateTime)dataReader["Date_of_Birth"];
+                // Convert DateTime to DateOnly
+                //DateOnly dateOnlyOfBirth = new DateOnly(dateOfBirth.Year, dateOfBirth.Month, dateOfBirth.Day);
+                u.DateOfBirth = dateOfBirth;
+
+                //u.DateOfBirth = (DateOnly)dataReader["Date_of_birth"];
+
+                u.Password = dataReader["Password"].ToString();
+                UsersList.Add(u);
+            }
+            return UsersList;
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        finally
+        {
+            if (con != null)
+            {
+                // close the db connection
+                con.Close();
+            }
+        }
+
+    }
+
+    //---------------------------------------------------------------------------------
+    // Create the SqlCommand using a stored procedure
+    //---------------------------------------------------------------------------------
+    private SqlCommand CreateUserCommandWithStoredProcedureWithoutParameters(String spName, SqlConnection con)
+    {
+
+        SqlCommand cmd = new SqlCommand(); // create the command object
+
+        cmd.Connection = con;              // assign the connection to the command object
+
+        cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+        cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+        cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be text
+
+        return cmd;
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    // This method Update User from the database 
+    //--------------------------------------------------------------------------------------------------
+    public int UpdateUser(User user)
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+
+        try
+        {
+            con = connect("myProjDB"); // create the connection
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        cmd = UpdateUserCommandWithStoredProcedureWithoutParameters("sp_LAL_UpdateUser", con, user);   // create the command
+
+        try
+        {
+            int numEffected = cmd.ExecuteNonQuery(); // execute the command
+            return numEffected;
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        finally
+        {
+            if (con != null)
+            {
+                // close the db connection
+                con.Close();
+            }
+        }
+
+    }
+
+    //---------------------------------------------------------------------------------
+    // Create the SqlCommand for UpdateUser using a stored procedure
+    //---------------------------------------------------------------------------------
+    private SqlCommand UpdateUserCommandWithStoredProcedureWithoutParameters(String spName, SqlConnection con, User user)
+    {
+
+        SqlCommand cmd = new SqlCommand(); // create the command object
+
+        cmd.Connection = con;              // assign the connection to the command object
+
+        cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+        cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+        cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be text
+
+        cmd.Parameters.AddWithValue("@Email", user.Email);
+        cmd.Parameters.AddWithValue("@First_name", user.FirstName);
+        cmd.Parameters.AddWithValue("@Last_name", user.LastName);
+        cmd.Parameters.AddWithValue("@Image", user.Image);
+        cmd.Parameters.AddWithValue("@Phone_Number", user.PhoneNumber);
+        cmd.Parameters.AddWithValue("@Date_of_Birth", user.DateOfBirth);
+        cmd.Parameters.AddWithValue("@Password", user.Password);
+
+        return cmd;
+    }
+
+}
