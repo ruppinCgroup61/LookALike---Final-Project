17a03f0709f813abcdb2df70332b6627eb6f4ad7
ruppinCgroup61 <159167415+ruppinCgroup61@users.noreply.github.com>
2024-08-19 21:41:25 +0300

    Delete LookALike Client directory

--- LookALike Client/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // base:"/cgroup61/test2/tar3/",
-})
